6db02f341f294135008a8e857dc8bed105143ec0
BadrOSX <BadrTM@hotmail.com>
2022-03-24 20:09:49 +0000

    Built most methods in GameStateFactory.java (Still needs changes after reading the cw github page) :( GameStateCreationTest.java passed

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -5,12 +5,13 @@ import com.google.common.collect.ImmutableList;
 import javax.annotation.Nonnull;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.UnmodifiableIterator;
+import org.controlsfx.control.ListSelectionView;
+import org.w3c.dom.Node;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -25,50 +26,48 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			ImmutableList<Player> detectives) {
 		// TODO
 
+		List<Player> Players = new ArrayList<>(){ {addAll(detectives); add(mrX);} };
+		List<Integer> detsLocations = detectives.stream()
+												.map(Player::location)
+												.toList();
+
 		// Check if there is no MrX
 		if (!mrX.isMrX()) {
 			System.out.println("No MRX");
 			throw new IllegalArgumentException();
 		}
+
+		// Check if any detective is also MrX (aka 2+ MrX's in the game) (JOIN WITH TOP IF LATER)
+		if (detectives.stream().anyMatch(Player::isMrX)) {
+			System.out.println("2+ MrX's in the game FIRST");
+			throw new IllegalArgumentException();
+		}
+
 		// Check for empty moves or graph
 		if (setup.moves.isEmpty() || setup.graph.nodes().isEmpty()) {
 			System.out.println("Empty Setup");
 			throw new IllegalArgumentException();
 		}
 
-		List<Number> detectivesLocations = new ArrayList<>();
-		List<Piece> detectivesPieces = new ArrayList<>();
-
-		for (Player detective : detectives) {
-			// Check if any detective is also MrX (aka 2+ MrX's in the game)
-			if (detective.isMrX()) {
-				System.out.println("2+ MrX's in the game");
-				throw new IllegalArgumentException();
-			}
-			// Check if any 2 detectives are assigned the same location/piece (Overlap)
-			if (detectivesLocations.stream().anyMatch(x -> (x.equals(detective.location()))) || detectivesPieces.stream().anyMatch(x -> x.equals(detective.piece()))) {
-				System.out.println("Detective Location/Piece Overlap");
-				throw new IllegalArgumentException();
-			}
-			// Check if detectives have illegal tickets
-			if (detective.has(ScotlandYard.Ticket.SECRET) || detective.has(ScotlandYard.Ticket.DOUBLE)) {
-				System.out.println("Detective Holds Illegal Ticket");
-				throw new IllegalArgumentException();
-			}
-			// Add detective location and piece to respective list to check with next detectives in case
-			// of overlap + get Players pieces(*can be improved)
-			detectivesLocations.add(detective.location());
-			detectivesPieces.add(detective.piece());
+		// Check if any 2 detectives are assigned the same location/piece (Overlap)
+		if (detectives.stream().anyMatch(x -> ((Collections.frequency(detectives, x) > 1) || (Collections.frequency(detsLocations, x.location()) > 1)))) {
+			System.out.println("Detective Location/Piece Overlap");
+			throw new IllegalArgumentException();
 		}
 
-		detectivesPieces.add(mrX.piece());
-		ImmutableSet<Piece> playersList = ImmutableSet.copyOf(detectivesPieces);
+		// Check if detectives have illegal tickets
+		if (detectives.stream().anyMatch(x -> x.has(ScotlandYard.Ticket.SECRET) || x.has(ScotlandYard.Ticket.DOUBLE))) {
+			System.out.println("Detective Holds Illegal Ticket");
+			throw new IllegalArgumentException();
+		}
 
-		GameState FactoryGameState = new GameState() {
+		// consider that the class can be private and final
+		GameState MyGameState = new GameState() {
 			@Nonnull
 			@Override
 			public GameState advance(Move move) {
-				return null;
+				GameState x = build(getSetup(), mrX, detectives);
+				return x;
 			}
 
 			@Nonnull
@@ -80,19 +79,42 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			@Nonnull
 			@Override
 			public ImmutableSet<Piece> getPlayers() {
-				return playersList;
+				return ImmutableSet.copyOf(Players.stream()
+													.map(Player::piece)
+													.collect(Collectors.toSet()));
 			}
 
 			@Nonnull
 			@Override
 			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
-				return Optional.empty();
+				// Get detective location by checking matching piece from detectives list, then
+				// calling the .location() method and passing
+				try {
+					Integer detLocation = detectives.stream()
+													.filter(x -> x.piece() == detective)
+													.toList()
+													.get(0)
+													.location();
+					return Optional.of(detLocation);
+				} catch (ArrayIndexOutOfBoundsException e){
+					return Optional.empty();
+				}
 			}
 
 			@Nonnull
 			@Override
 			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
-				return Optional.empty();
+				if (Players.stream().anyMatch(x -> x.piece() == piece)) {
+					TicketBoard playerTicket = ticket -> Players.stream()
+																.filter(x -> x.piece() == piece)
+																.toList()
+																.get(0)
+																.tickets()
+																.get(ticket);
+					return Optional.of(playerTicket);
+				} else {
+					return Optional.empty();
+				}
 			}
 
 			@Nonnull
@@ -104,17 +126,46 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			@Nonnull
 			@Override
 			public ImmutableSet<Piece> getWinner() {
-				return null;
+				ImmutableSet<Piece> gameWinner = ImmutableSet.of();
+				return gameWinner;
 			}
 
 			@Nonnull
 			@Override
 			public ImmutableSet<Move> getAvailableMoves() {
-				return null;
+
+				List<Move> moveList = new ArrayList<>();
+
+				//CHECK VISITOR PATTERN
+
+				for (Player detective: detectives){
+					// Current piece
+					Piece curPiece = detective.piece();
+
+					// Piece location
+					int curLocation = detective.location();
+
+					// Ticket values for TAXI, BUS and UNDERGROUND respectively for each detective piece
+					ImmutableList<Integer> xTicketLeft = detective.tickets().values().asList();
+
+					// TAXI ticket loop
+					for (int i = 0; i < xTicketLeft.get(0); i++) {
+						moveList.add(new Move.SingleMove(curPiece, curLocation, ScotlandYard.Ticket.TAXI, i));
+					}
+					// BUS ticket loop
+					for (int i = 0; i < xTicketLeft.get(1); i++) {
+						moveList.add(new Move.SingleMove(curPiece, curLocation, ScotlandYard.Ticket.BUS, i));
+					}
+					// UNDERGROUND ticket loop
+					for (int i = 0; i < xTicketLeft.get(2); i++) {
+						moveList.add(new Move.SingleMove(curPiece, curLocation, ScotlandYard.Ticket.UNDERGROUND, i));
+					}
+				}
+				return ImmutableSet.copyOf(moveList);
 			}
 		};
 
-		return FactoryGameState;
+		return MyGameState;
 	}
 
 }
--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,10 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.Optional;
 
 /**
  * cw-model
@@ -16,6 +19,78 @@ public final class MyModelFactory implements Factory<Model> {
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
 		// TODO
+
+		Model gameModel = new Model() {
+			@Nonnull
+			@Override
+			public Board getCurrentBoard() {
+				Board currentBoard = new Board() {
+					@Nonnull
+					@Override
+					public GameSetup getSetup() {
+						return null;
+					}
+
+					@Nonnull
+					@Override
+					public ImmutableSet<Piece> getPlayers() {
+						return null;
+					}
+
+					@Nonnull
+					@Override
+					public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+						return Optional.empty();
+					}
+
+					@Nonnull
+					@Override
+					public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+						return Optional.empty();
+					}
+
+					@Nonnull
+					@Override
+					public ImmutableList<LogEntry> getMrXTravelLog() {
+						return null;
+					}
+
+					@Nonnull
+					@Override
+					public ImmutableSet<Piece> getWinner() {
+						return null;
+					}
+
+					@Nonnull
+					@Override
+					public ImmutableSet<Move> getAvailableMoves() {
+						return null;
+					}
+				};
+				return null;
+			}
+
+			@Override
+			public void registerObserver(@Nonnull Observer observer) {
+
+			}
+
+			@Override
+			public void unregisterObserver(@Nonnull Observer observer) {
+
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Observer> getObservers() {
+				return null;
+			}
+
+			@Override
+			public void chooseMove(@Nonnull Move move) {
+
+			}
+		};
 		throw new RuntimeException("Implement me!");
 	}
 }
